dbfcacbdac7df7b149d1b831f769d20e6c5719b3
pan.sun <1163927457@qq.com>
2016-12-27 14:19:07 +0800

    新增接口用于对接大文件上传

--- src/main/java/com/tpadsz/img/api/ImageManager.java
+++ src/main/java/com/tpadsz/img/api/ImageManager.java
@@ -6,13 +6,22 @@ import com.tpadsz.exception.SystemAlgorithmException;
 import com.tpadsz.img.vo.ImageOffer;
 
 public interface ImageManager {
-	
+
 	public boolean delete(ImageOffer offer);
 
 	String storageURL(ImageOffer offer, @NotBlank String url) throws SystemAlgorithmException;
 
 	String storageLocalFile(ImageOffer offer, String file)	throws SystemAlgorithmException;
-	
+
 	String storageStr(ImageOffer offer, String file) throws SystemAlgorithmException;
+
+	String deleteTaskResult(String publish_url,String md5)	throws SystemAlgorithmException;
+	String getTaskStatus(String taskId) throws  SystemAlgorithmException;
+	String storageURL(ImageOffer offer, @NotBlank String url,long time) throws SystemAlgorithmException;
+
+	String storageLocalFile(ImageOffer offer, String file,long time)	throws SystemAlgorithmException;
+
+	String storageStr(ImageOffer offer, String file,long time) throws SystemAlgorithmException;
+
 	
 }
